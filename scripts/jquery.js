db6b12b48ee24d58dab11b21b780b105679e5eb0
duggiemitchell <erica.carter27@gmail.com>
2015-12-15 15:18:34 -0500

    load files for project page

---
+++ scripts/jquery.js
@@ -0,0 +1,119 @@
+
+    var happyQuotes = [
+
+		//“Do not cry because its over, smile because it happened.	― Dr. Seuss",
+		"Love is that condition in which the happiness of another person is essential to your own.  ― Robert A. Heinlein, Stranger in a Strange Land",
+		"Folks are usually about as happy as they make their minds up to be.   ―Abraham Lincoln",
+		"Its so hard to forget pain, but its even harder to remember sweetness. We have no scar to show for happiness. We learn so little from peace. 	― Chuck Palahniuk",
+		"Happiness is when what you think, what you say, and what you do are in harmony.	― Mahatma Gandhi",
+		"You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life.	― Albert Camus",
+		"Every man has his secret sorrows which the world knows not; and often times we call a man cold when he is only sad.  ―Henry Wadsworth Longfellow",
+		"Count your age by friends, not years. Count your life by smiles, not tears.   ― John Lennon",
+		"There is nothing like deep breaths after laughing that hard. Nothing in the world like a sore stomach for the right reasons.   ― Stephen Chbosky, The Perks of Being a Wallflower",
+		"The most important thing is to enjoy your life—to be happy—it's all that matters. ― Audrey Hepburn",
+		"Happiness is a warm puppy.  ― Charles M. Schulz",
+		"It isn't what you have or who you are or where you are or what you are doing that makes you happy or unhappy. It is what you think about it.  ― Dale Carnegie, How to Win Friends and Influence People",
+		"The only way to find true happiness is to risk being completely cut open.	― Chuck Palahniuk, Invisible Monsters",
+		"Let us be grateful to the people who make us happy; they are the charming gardeners who make our souls blossom.  ― Marcel Proust",
+		"You can't be happy unless you\'re unhappy sometimes.  ― Lauren Oliver, Delirium",
+		"Hope. Smiles from the threshold of the year to come,  Whispering 'it will be happier '... ― Alfred Lord Tennyson",
+		"Sanity and happiness are an impossible combination.  ― Mark Twain",
+
+	];
+
+  var sadQuotes = [
+		"There is a sacredness in tears. They are not the mark of	weakness, but of power. They speak more eloquently than ten thousand tongues. They are the messengers of overwhelming grief, of deep contrition, and of unspeakable love.   -Washington Irving",
+		"The word 'happiness' would lose its meaning if it were not balanced by sadness.   -Carl Jung",
+		"I\'ve cried, and you'd think I'd be better for it, but the sadness just sleeps, and it stays in my spine the rest of my life.   -Conor Oberst",
+		"Heavy hearts, like heavy clouds in the sky, are best relieved by the letting of a little water.   -Christopher Morley",
+		"Every human walks around with a certain kind of sadness. They may not wear it on their sleeves, but it\'s there if you look deep.   - Taraji P. Henson",
+		"I really believe that all of us have a lot of darkness in our souls. Anger, rage, fear, sadness. I don't think that's only reserved for people who have horrible upbringings. I think it really exists and is part of the human condition. I think in the course of your life you figure out ways to deal with that.   -Kevin Bacon",
+		"Tears come from the heart and not from the brain.  -Leonardo da Vinci",
+		"Your pain is the breaking of the shell that encloses your understanding.  -William Cowper",
+		"Sadness flies away on the wings of time.   -Jean de La Fontaine",
+		"We're taught to be ashamed of confusion, anger, fear and sadness, and to me they're of equal value to happiness, excitement and inspiration.  -Alanis Morissette",
+		"Oh I am very weary, Though tears no longer flow; My eyes are tired of weeping, My heart is sick of woe.  -Anne Bronte",
+		"Our sweetest songs are those that tell of saddest thought.   -Percy Bysshe Shelley",
+		"The walls we build around us to keep sadness out also keeps out the joy.   -Jim Rohn",
+		"I do believe that if you haven't learnt about sadness, you cannot appreciate happiness.   -Nana Mouskouri",
+		"Experiencing sadness and anger can make you feel more creative, and by being creative, you can get beyond your pain or negativity.   -Yoko Ono",
+		"Bad things do happen; how I respond to them defines my character and the quality of my life. I can choose to sit in perpetual sadness, immobilized by the gravity of my loss, or I can choose to rise from the pain and treasure the most precious gift I have - life itself.   -Walter Anderson",
+		"When I despair, I remember that all through history the way of truth and love have always won. There have been tyrants and murderers, and for a time, they can seem invincible, but in the end, they always fall. Think of it--always.   ― Mahatma Gandhi",
+
+];
+
+	var lostQuotes = [
+    "Not all those who wander are lost.   -Tolkien",
+    "There are two tragedies in life. One is to lose your heart's desire. The other is to gain it.  ― George Bernard Shaw",
+    "In this world, it is too common for people to search for someone to lose themselves in. But I am already lost. I will look for someone to find myself in.   ― C. JoyBell C.",
+    "Weeping is not the same thing as crying. It takes your whole body to weep, and when it's over, you feel like you don't have any bones left to hold you up.  ― Sarah Ockler",
+    "“If you feel lost, disappointed, hesitant, or weak, return to yourself, to who you are, here and now and when you get there, you will discover yourself, like a lotus flower in full bloom, even in a muddy pond, beautiful and strong.",
+    "Getting lost is just another way of saying 'going exploring.  ― Justina Chen",
+    "There is love of course. And then there's life, its enemy.  ― Justina Chen   ― Jean Anouilh",
+    "I realise now that I wanted to disappear. To get so lost that nobody ever found me. To go so far away that I'd never be able to make my way home again. But I have no idea why.  ― Jessica Warman,",
+    "Losing is part of the game. If you never lose, you are never truly tested, and never forced to grow.   -Davic Sirlin",
+    "Losing is only temporary and not all encompassing. You must simply study it, learn from it, and try hard not to lose the same way again. Then you must have the self-control to forget about it.  -John Wooden",
+    "If you learn from a loss you have not lost.   -Austin O'Malley",
+    "Winners have to absorb losses.   -Ice-T",
+    "Sometimes you lose. Nothing you can do but admit it.  -Sarah Dessen",
+    "For every winner, there's a loser. And that person didn't really need to lose. They just didn't understand the game plan.   -Buzz Alrin",
+  ]
+	var scaredQuotes = [
+
+    "Do one thing every day that scares you.  ― Eleanor Roosevelt",
+    "There is only one thing that makes a dream impossible to achieve: the fear of failure.  ― Paulo Coelho",
+    "I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.   must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.   -Dune",
+    "Fear doesn't shut you down; it wakes you up.   ― Veronica Roth ",
+    "Fear cuts deeper than swords.   ― George R.R. Martin",
+    "If you know the enemy and know yourself you need not fear the results of a hundred battles.   - Sun Tzu",
+    "Thinking will not overcome fear but action will.   -W. Clement Stone",
+    "The oldest and strongest emotion of mankind is fear, and the oldest and strongest kind of fear is fear of the unknown.   _H.P. Lovecraft",
+    "I'm not afraid of storms, for I'm learning how to sail my ship.   -Jiddu Krishnamurti",
+    "Have no fear of perfection - you'll never reach it.   -Salvador Dali",
+    "I have learned over the years that when one's mind is made up, this diminishes fear; knowing what must be done does away with fear.   -Rosa Parks",
+    "Always do what you are afraid to do.   -Ralph Waldo Emerson",
+    "We consume our tomorrows fretting about our yesterdays.   -Persius",
+    "Even death is not to be feared by one who has lived wisely.   -Buddha",
+    "We should not fret for what is past, nor should we be anxious about the future; men of discernment deal only with the present moment.   -Chanakya",
+    "To pity distress is but human; to relieve it is Godlike.   Horace Mann",
+    "That man is prudent who neither hopes nor fears anything from the uncertain events of the future.   -Anatole France",
+    "Humble souls are fearful of their own strength.   -William Gurnall"  ];
+
+
+
+      function getRandomNumber(min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
+      function getRandomQuote(type){
+        var quote;
+
+        switch(type) {
+          case "happy":
+            quote = happyQuotes[getRandomNumber(0, happyQuotes.length)];
+            break;
+          case "sad":
+            quote = sadQuotes[getRandomNumber(0, sadQuotes.length)];
+            break;
+          case "lost":
+            quote = lostQuotes[getRandomNumber(0, lostQuotes.length)];
+            break;
+          case "scared":
+            quote = scaredQuotes[getRandomNumber(0, scaredQuotes.length)];
+            break;
+        }
+
+        return quote;
+      }
+
+      $( document ).ready(function() {
+
+
+      $("button").on( "click", function(e) {
+
+          var quote = getRandomQuote(e.target.getAttribute('feeling'));
+        	$('.quoteSection').text(quote);
+          $('.quoteSection').show('slow');
+      });
+
+      });
